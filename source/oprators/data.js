31e8a230984e1ff2e64e80be6b266f1b5d147220
guesthom <ndsnaren@gmail.com>
2020-02-11 01:10:40 +0530

    [Add] images

--- source/oprators/data.js
+++ source/oprators/data.js
@@ -1,9 +1,9 @@
 export const oprators = [
     { key: '1', name: 'Tata Sky', image: require('../../assets/images/oprators/tatasky.jpg') },
     { key: '2', name: 'Dish TV', image: require('../../assets/images/oprators/dishtv.png') },
-    { key: '3', name: 'Airtel Digital TV (ADTV)', image: require('../../assets/images/categories/sport.png') },
-    { key: '4', name: 'Videocon D2H', image: require('../../assets/images/categories/music.png') },
-    { key: '5', name: 'Independent (Reliance Digita) TV', image: require('../../assets/images/categories/kids.png') },
+    { key: '3', name: 'Airtel Digital TV (ADTV)', image: require('../../assets/images/oprators/airtel.png') },
+    { key: '4', name: 'Videocon D2H', image: require('../../assets/images/oprators/videocon.jpg') },
+    { key: '5', name: 'Independent (Reliance Digita) TV', image: require('../../assets/images/oprators/relince.jpg') },
     { key: '6', name: 'Sun Direct', image: require('../../assets/images/categories/news.png') },
     { key: '7', name: 'DD Free Dish', image: require('../../assets/images/categories/education.png') },
 ]
